99bc7e0d8b89627f2374d33af21e0478cf6eb388
jatobro <jakob.tossebro@gmail.com>
2026-02-16 20:56:55 +0100

    add back format config

--- lua/jatobro/lazy/colors.lua
+++ lua/jatobro/lazy/colors.lua
@@ -17,7 +17,16 @@ return {
           italic = false,
         },
       }
-
+    end,
+  },
+  {
+    'vague-theme/vague.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      require('vague').setup {
+        transparent = true,
+      }
       ColorMyPencils()
     end,
   },
